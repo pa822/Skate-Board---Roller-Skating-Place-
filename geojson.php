28b651eda3e294cb75a767500ca9528127e9bf5b
Prashant Patel <prashant.a822@gmail.com>
2018-04-09 22:34:44 +0000

    {

--- geojson.php
+++ geojson.php
@@ -1,7 +1,7 @@
 <?php
 // kudos to http://stackoverflow.com/a/18106727/1778785 for snippet of PHP to read Google spreadsheet as CSV
 
-$googleSpreadsheetUrl = "https://docs.google.com/spreadsheet/pub?key=10eNXFh6mzFtii7B2PW90jmHtrQLJlRCrf3kkHU0HIH8&single=true&gid=0&output=csv";
+$googleSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS2vZyIaATWko19MY5eTLxQxKmO4VSCiw-4kNk6QblsCxwibpmabpSAptMpR0d56yc0SNnJPWgnNMtW/pub?output=csv";
 
 $rowCount = 0;
 $features = array();
